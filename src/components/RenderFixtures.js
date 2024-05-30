fdbb65975ec3b877e05318d50d8b3cbdd7e2d1bf
Scott Hahn <scotthahn2210@gmail.com>
2024-05-30 10:21:40 +0200

    added error message for when fixtures list is empty

--- src/components/RenderFixtures.js
+++ src/components/RenderFixtures.js
@@ -18,39 +18,49 @@ const RenderFixtures = ({ sortByTime, teams, loading, setLoading }) => {
 
     return (
         loading === false ? 
-        newFixtures.length > 0 && 
+        newFixtures.length > 0 ?
         newFixtures.map((item, index) =>
             <div key={index} className='fixtures'>
                 <h1 id='fixture-date'>{sortByTime[index][0].date.toDateString()}</h1>
                 { sortByTime[index].map((item, index) => (
                     <div key={`${index} ${index += 1}`} className='fixtures-container'>
-                        <p key={`${index} ${index += 2}`}>
+                        <p>
                             {item.homeTeam}
                         </p>
                         { 
                             teams.filter(team => team.name === item.homeTeam)
                             .map(team => <img key={`${index} ${index += 3}`} className='home-badge' src={team.badge} /> ) 
                         }
-                        <p key={`${index} ${index += 4}`}> 
+                        <p> 
                             { Array.from(item.time).slice(0, 5) } 
                         </p>
                         { 
                             teams.filter(team => team.name === item.awayTeam)
                             .map(team => <img key={`${index} ${index += 5}`} className='away-badge' src={team.badge} /> ) 
                         }
-                        <p key={`${index} ${index += 6}`}>
+                        <p>
                             {item.awayTeam}
                         </p>
-                        <img key={`${index} ${index += 7}`} 
-                            width='50px' src='https://cdn4.iconfinder.com/data/icons/buildings-and-structures-3/512/sports___stadium_soccer_football_fitness_building.png' 
+                        <img 
+                            width='50px' 
+                            src='https://cdn4.iconfinder.com/data/icons/buildings-and-structures-3/512/sports___stadium_soccer_football_fitness_building.png' 
                         />
-                        <p key={`${index} ${index += 8}`}>
+                        <p>
                             {item.venue}
                         </p>
                     </div>
                 ))}
             </div>
-        )
+        ) 
+        :
+            <>
+                <h1 style={{ textAlign: "center", fontFamily: "Arial" }}>
+                    Season hasn't started yet! 
+                </h1>
+                <h2 style={{ textAlign: "center", fontFamily: "Arial" }}>
+                    All Premier League fixtures for 2024-25 will be released on June 18, 2024.
+                </h2>
+            </>
         : 
         <div className='loading-container'>
             <div className='loading'></div>
