14719a13e5db840ffc255ad5b35050a7ea7b9040
Scott Hahn <scotthahn2210@gmail.com>
2024-05-30 18:54:58 +0200

    removed highlights

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -5,45 +5,31 @@ import parse from 'html-react-parser';
 const Highlights = () => {
     const [videos, setVideos] = useState([]);
 
-    useEffect(() => {
-        const options = {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': 'ead11d69acmsh1705e43a9fa2f97p1c4389jsn945fedb1ff12',
-                'X-RapidAPI-Host': 'free-football-soccer-videos.p.rapidapi.com'
-            }
-        };
-        
-        fetch('https://free-football-soccer-videos.p.rapidapi.com/', options)
-        .then(response => response.json())
-        .then(data => data.map(item => {
-            item.competition.name === "ENGLAND: Premier League" && 
-            setVideos(prev => [...prev,
-                { 
-                    title: item.title, 
-                    vid: item.videos[0].embed, 
-                    date: new Date(item.date) 
-                }])
-        }));
-    }, []);
-
+    // useEffect(() => {
+    //     fetch('http://localhost:8500/highlights')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         setVideos(data.filter(item => item.competition.name === 'ENGLAND: Premier League'));
+    //     })
+    // }, []);
+    
     return (  
         <div className='highlights-container'>
-                {videos.length > 0 && <h1 id='highlights'>Highlights</h1>}
-                <div className='highlights'>
-                    {videos.map((item, index) => (
-                        <div key={index}> 
-                          { parse(item.vid.toString())} 
-                          <h4 key={`${index} ${index++}`}>{item.title}</h4>  
-                          <hr></hr>
-                          <strong> 
-                            <p key={`${index} ${index +=2}`} style={{color: 'rgb(233,0,82)'}}>
-                                { item.date.toString().slice(0, 15) }
+            {/* {videos.length > 0 && <h1 id='highlights'>Highlights</h1>}
+            <div className='highlights'>
+                {videos.map((item, index) => (
+                    <div key={index}> 
+                        { parse(item.videos[0].embed.toString())} 
+                        <h4>{item.title}</h4>  
+                        <hr></hr>
+                        <strong> 
+                            <p style={{color: 'rgb(233,0,82)'}}>
+                                { new Date(item.date).toString().slice(0, 15) }
                             </p> 
-                          </strong>
-                        </div>
-                    ))}
-                </div>
+                        </strong>
+                    </div>
+                ))}
+            </div> */}
         </div>
     )
 };
