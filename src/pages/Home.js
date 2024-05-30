1f9b0531238fcb38099475cfd3d2ebb74a57f964
Scott Hahn <scotthahn2210@gmail.com>
2024-05-30 05:53:36 +0200

    changed search query for news

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,34 +6,33 @@ import HomeTable from '../components/HomeTable';
 import HomeFixtures from '../components/HomeFixtures';
 
 const Home = () => {
-    const [article, setArticle] = useState([]);
-    const [randomTeam, setRandomTeam] = useState('');
-    const teams = ['Arsenal', 'Chelsea', 'Liverpool', 'Manchester City', 'Manchester United', 'Tottenham'];
+    const [article, setArticle] = useState();
 
     useEffect(() => {
-        setRandomTeam(teams[Math.floor(Math.random() * teams.length)]);
-    }, []);
-
-    useEffect(() => {
-        randomTeam !== '' &&
-        fetch(`https://gnews.io/api/v4/top-headlines?lang=en&q=${randomTeam}&token=2f867f8e9ffd6c133b2f527baec2de7b`)
+        fetch(`https://gnews.io/api/v4/top-headlines?lang=en&q=premier%20league&token=2f867f8e9ffd6c133b2f527baec2de7b`)
         .then(response => response.json())
         .then(data => {
-            setArticle({ image: data.articles[0].image, title: data.articles[0].title, description: data.articles[0].description, link: data.articles[0].url, source: data.articles[0].source.name })
+            setArticle(data.articles[Math.floor(Math.random() * data.articles.length)]);
         });
-    }, [randomTeam]);
+    }, []);
 
     return (
         <section className='home'>
             <div className='home-main-container'>
-                {article.image !== null && <img id='headline-img' src={article.image} onClick={() => window.open(article.link)} alt='' />}
+                {
+                    article && (
+                    <>
+                        {article.image !== null && <img id='headline-img' src={article.image} onClick={() => window.open(article.url)} alt='' />}
 
-                <div className='home-main'>
-                    <h4 style={{color: 'rgb(4,245,255)'}}>Top Story</h4>
-                    <h2>{article.title}</h2>
-                    <p>{article.description}</p>
-                    <h4>Source - {article.source}</h4>
-                </div>
+                        <div className='home-main'>
+                            <h4 style={{color: 'rgb(4,245,255)'}}>Top Story</h4>
+                            <h2>{article.title}</h2>
+                            <p>{article.description}</p>
+                            <h4>Source - {article.source.name}</h4>
+                        </div>
+                    </>
+                    )
+                }
 
             </div>
             
