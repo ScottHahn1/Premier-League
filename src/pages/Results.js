f5e3d358bec7150b0a21d3be2009e40f4a62c354
Scott Hahn <scotthahn2210@gmail.com>
2024-05-30 12:49:46 +0200

    changed results to be sorted by date

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -7,15 +7,11 @@ import '../styles/Results.css';
 
 const Results = () => {
     const [results, setResults] = useState([]);
-    const [startingRound, setStartingRound] = useState(0);
     const [teams, setTeams] = useState([]);
-    const [rounds, setRounds] = useState([
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38
-    ]);
+    const rounds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38];
     const [loading, setLoading] = useState(true);
 
-
-    let groupResults = results.filter(result => result.date < new Date()).sort((a, b) => a.round - b.round).reduce(function (a, b) {
+    let groupResults = results.filter(result => result.date < new Date()).sort((a, b) => new Date(a.date) - new Date(b.date)).reduce(function (a, b) {
         a[b.date] = a[b.date] || [];
         a[b.date].push(b);
         return a;
@@ -26,7 +22,7 @@ const Results = () => {
 
     return (
         <div>
-            <GetResults setStartingRound={setStartingRound} setResults={setResults} rounds={rounds} />
+            <GetResults setResults={setResults} rounds={rounds} />
             <GetTeams setTeams={setTeams} />
 
             <div className='results-heading'>
