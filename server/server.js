afa9fd7920f4e2d59cce6dfbd39266170da6cd35
Scott Hahn <scotthahn2210@gmail.com>
2024-05-31 16:27:22 +0200

    added routes

--- server/server.js
+++ server/server.js
@@ -2,15 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const { highlightsRouter } = require('./routes/highlights');
 const dotenv = require('dotenv');
+const { newsRouter } = require('./routes/news');
+const { gamesRouter } = require('./routes/games');
 
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT || 8500;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
 
 app.use('/highlights', highlightsRouter);
+app.use('/news', newsRouter);
+app.use('/games', gamesRouter);
 
 app.listen(PORT, () => console.log('Server started'));
