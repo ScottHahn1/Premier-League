88a48b0a437cf4f22f50a48f688b9d7a619c8e50
Scott Hahn <scotthahn2210@gmail.com>
2024-05-31 16:25:54 +0200

    added file

---
+++ server/routes/games.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const gamesRouter = express.Router();
+
+gamesRouter.get('/fixtures/:round', async (req, res) => {
+    const response = await fetch(`https://www.thesportsdb.com/api/v1/json/3/eventsround.php?id=4328&r=${req.params.round}&s=2023-2024`);
+    const data = await response.json();
+    res.status(200).send(data);
+});
+
+gamesRouter.get('/results/:round', async (req, res) => {
+    const response = await fetch(`https://www.thesportsdb.com/api/v1/json/3/eventsround.php?id=4328&r=${req.params.round}&s=2023-2024`);
+    const data = await response.json();
+    res.status(200).send(data);
+});
+
+module.exports = { gamesRouter };
---
+++ server/routes/news.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const newsRouter = express.Router();
+
+newsRouter.get('/', async (req, res) => {
+   const response = await fetch('https://site.api.espn.com/apis/site/v2/sports/soccer/eng.1/news');
+   const data = await response.json();
+   res.status(200).send(data);
+});
+
+newsRouter.get('/article', async (req, res) => {
+    const response = await fetch(`https://gnews.io/api/v4/top-headlines?lang=en&q=premier%20league&token=${process.env.REACT_APP_NEWS_API_KEY}`);
+    const data = await response.json();
+    res.status(200).send(data);
+});
+
+module.exports = { newsRouter };
