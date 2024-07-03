4b9e8749af59ef7b3423200f258e8150bc38f174
Scott Hahn <scotthahn2210@gmail.com>
2024-07-03 11:28:30 +0200

    changed fixtures api call to 2024-2025 season

--- server/routes/games.js
+++ server/routes/games.js
@@ -6,7 +6,7 @@ dotenv.config();
 const gamesRouter = express.Router();
 
 gamesRouter.get('/fixtures/:round', async (req, res) => {
-    const response = await fetch(`https://www.thesportsdb.com/api/v1/json/3/eventsround.php?id=4328&r=${req.params.round}&s=2023-2024`);
+    const response = await fetch(`https://www.thesportsdb.com/api/v1/json/3/eventsround.php?id=4328&r=${req.params.round}&s=2024-2025`);
     const data = await response.json();
     res.status(200).send(data);
 });
