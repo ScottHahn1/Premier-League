a03051e6f341fd183f9185d4aa68f1168b0359b6
ScottHahn1 <61561620+ScottHahn1@users.noreply.github.com>
2024-09-23 18:32:09 +0200

    Changed table data API call to 2024-2025 season

--- server/routes/stats.js
+++ server/routes/stats.js
@@ -6,7 +6,7 @@ dotenv.config();
 const statsRouter = express.Router();
 
 statsRouter.get('/table', async (req, res) => {
-    const response = await fetch(`https://www.thesportsdb.com/api/v1/json/3/lookuptable.php?l=4328&s=2023-2024`);
+    const response = await fetch(`https://www.thesportsdb.com/api/v1/json/3/lookuptable.php?l=4328&s=2024-2025`);
     const data = await response.json();
     res.status(200).send(data);
 });
