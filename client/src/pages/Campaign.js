7f6810502cfa301db73a4ec8ec4d4f5926196349
ScottHahn1 <scotthahn2210@gmail.com>
2024-10-22 22:08:49 +0200

    replaced video

--- client/src/pages/Campaign.js
+++ client/src/pages/Campaign.js
@@ -18,7 +18,7 @@ const Campaign = () => {
             Not online, not in the stands, not at home and not on the streets. <br></br>
             We all must come together as fans, as players and as clubs to combat discrimination wherever it exists in society.
           </h4>
-            <iframe id='video' title="vimeo-player" src="https://player.vimeo.com/video/637524637?h=10d3e8136d" width="650" height="600" frameborder="0" allowfullscreen></iframe>
+            <iframe id='video' title="vimeo-player" src={`https://youtube.com/embed/DhKTndooD-k?autoplay=0`} width="650" height="600" allowFullScreen></iframe>
             <p>Through No Room For Racism, the Premier League and its clubs work with fans, the FA, EFL, PFA, Kick It Out and the police to tackle racism on and off the pitch, promoting equality, diversity and inclusion across all areas of football.</p>
             <p>Together we can all do more to make a positive impact.</p>
             <p>So, if you see discrimination, challenge it, report it, change it.</p>
@@ -35,7 +35,7 @@ const Campaign = () => {
             <div className='banner'>
             </div>
 
-            <img src='https://resources.premierleague.com/photos/2021/10/07/46f0aa45-3484-4cdb-96f2-92cae1277994/NRFR-sleeve-badge-B-W.jpg?width=600&height=333' />
+            <img src='https://resources.premierleague.com/photos/2021/10/07/46f0aa45-3484-4cdb-96f2-92cae1277994/NRFR-sleeve-badge-B-W.jpg?width=600&height=333' alt='Action plan' />
           </div>
 
           <br></br>
@@ -51,7 +51,7 @@ const Campaign = () => {
             <div className='banner'>
             </div>
 
-            <img src='https://resources.premierleague.com/photos/2020/10/15/c1ae4019-04c1-4f61-b3b9-a8b81b87401a/Reporting-abuse-Fan-on-phone-v2-cropped-B-W.jpg?width=600&height=333' />
+            <img src='https://resources.premierleague.com/photos/2020/10/15/c1ae4019-04c1-4f61-b3b9-a8b81b87401a/Reporting-abuse-Fan-on-phone-v2-cropped-B-W.jpg?width=600&height=333' alt='Reporting racism' />
           </div>
 
         </div>
