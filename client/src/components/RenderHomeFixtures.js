160283b11cb2f083acc9bedf5398b26a12c39882
ScottHahn1 <scotthahn2210@gmail.com>
2024-11-25 11:28:25 +0200

    refactor: simplified RenderHomeFixtures by removing redundant state and integrating props for improved clarity and efficiency.

--- client/src/components/RenderHomeFixtures.js
+++ client/src/components/RenderHomeFixtures.js
@@ -1,59 +1,50 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
 import '../styles/Home.css';
 
-const RenderHomeFixtures = ({sortByTime, teams, startingRound, loading, setLoading}) => {
-    const [fixtures, setFixtures] = useState([]);
-
-    useEffect(() => {
-        sortByTime.length && startingRound.length && sortByTime.map((item, index) => {
-            item[index] !== undefined && item[index].round === startingRound[startingRound.length - 1] && setFixtures(prev => [...prev, item[index]]);
-        })
-    }, [startingRound]);
-
-    let newFixtures = fixtures.length && startingRound.length && 
-    [
-      ...new Set(fixtures.sort((a, b) => a.round - b.round)
-      .filter(item => item.round === startingRound[startingRound.length - 1]))
-    ];
-
-    useEffect(() => {
-      teams.length && setLoading(false)
-    });
-
+const RenderHomeFixtures = ({ fixtures, teams, loading }) => {
     return (
-      loading === false ?
-      newFixtures.length > 0 && 
-      sortByTime.length && newFixtures.map((item, index) =>
-          <div key={`${index} ${index + 1}`} className='home-fixtures'>
+      loading === false &&
+      fixtures.slice(0, 3).map((fixtureGroup, index) =>
+        <div key={fixtureGroup[0].idEvent} className='home-fixtures'>
+          <h3 id='home-fixture-date'>
+            { new Date(fixtures[index][0].dateEvent).toDateString() }
+          </h3>
 
-              <h3 id='home-fixture-date'>{sortByTime[index][0].date.toDateString()}</h3>
-              { sortByTime[index].map(item => (
-                  <div key={`${index} ${index++}`} className='home-fixtures-container'>
+          {fixtures[index].map(fixture => (
+            <div key={fixture.idEvent} className='home-fixtures-container'>
+                {
+                  teams.filter(team => team.name === fixture.strHomeTeam)
+                  .map(team => team.abbName === null ? 
+                    <p key={`${fixture.idEvent}-home-abb`}>{ fixture.strHomeTeam.slice(0, 3).toUpperCase() }</p> : 
+                    <p key={`${fixture.idEvent}-home-abb`}>{ team.abbName }</p>
+                  )
+                }
 
-                      { teams.filter(team => team.name === item.homeTeam)
-                        .map(team => team.abbName === null ? <p key={`${index}0`}>
-                        { item.homeTeam.slice(0, 3).toUpperCase() }</p> : <p key={`${index}`}>{ team.abbName }</p>)}
+                {
+                  teams.filter(team => team.name === fixture.strHomeTeam)
+                  .map(team => <img key={`${fixture.idEvent}-home-badge`} width='40px' height='40px' src={team.badge} alt={`${team.name}'s badge`} />) 
+                }
 
-                      { teams.filter(team => team.name === item.homeTeam)
-                        .map(team => <img key={`${index}1`} width='40px' height='40px' src={team.badge} /> ) 
-                      }
+                <p> 
+                  { Array.from(fixture.strTime).slice(0, 5) } 
+                </p>
 
-                      <p key={`${index}2`}> { Array.from(item.time).slice(0, 5) } </p>
+                {
+                  teams.filter(team => team.name === fixture.strAwayTeam)
+                  .map(team => <img key={`${fixture.idEvent}-away-badge`} width='40px' height='40px' src={team.badge} alt={`${team.name}'s badge`} />) 
+                }
 
-                      { teams.filter(team => team.name === item.awayTeam)
-                        .map(team => <img key={`${index}3`} width='40px' height='40px' src={team.badge} /> ) }
-
-                      { teams.filter(team => team.name === item.awayTeam)
-                        .map(team => team.abbName === null ? <p key={`${index}4`}>
-                        { item.awayTeam.slice(0, 3).toUpperCase() }</p> : <p key={`${index}4`}>{ team.abbName }</p>)}
-
-                  </div>
-              ))}
-          </div>
+                {
+                  teams.filter(team => team.name === fixture.strAwayTeam)
+                  .map(team => team.abbName === null ? 
+                    <p key={`${fixture.idEvent}-away-abb`}>{ fixture.strAwayTeam.slice(0, 3).toUpperCase() }</p> : 
+                    <p key={`${fixture.idEvent}-away-abb`}>{ team.abbName }</p>
+                  )
+                }
+            </div>
+          ))}
+        </div>
       )
-      :
-      null
     )
 };
 
