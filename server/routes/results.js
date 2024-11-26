65b2878e2b6dafd3c45c70f92bf7d49c5e082372
ScottHahn1 <scotthahn2210@gmail.com>
2024-11-26 17:14:44 +0200

    feature: added results route for fetching data for previous played matches

---
+++ server/routes/results.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const resultsRouter = express.Router();
+
+resultsRouter.get('/', async (req, res) => {
+    const allRoundsData = [];
+
+    for (let round = 1; round <= 20; round++) {
+        const response = await fetch(`https://www.thesportsdb.com/api/v1/json/3/eventsround.php?id=4328&r=${round}&s=2024-2025`);
+        const data = await response.json();
+        await data.events && allRoundsData.push(...data.events);
+    }
+ 
+    const results = allRoundsData.filter(event => {
+        return new Date(event.dateEvent).toISOString().slice(0, 10) <= new Date().toISOString().slice(0, 10);
+    })
+
+    let groupedResults = results.reduce(function (a, b) {
+        a[b.dateEvent] = a[b.dateEvent] || [];
+        a[b.dateEvent].push(b);
+        return a;
+    }, Object.create(null));
+
+    let timeSortedResults = Object.values(groupedResults).map(result => {
+        return result.sort((a, b) => a.strTime.localeCompare(b.strTime));
+    });
+    
+    res.status(200).send(timeSortedResults);
+});
+
+module.exports = { resultsRouter };
