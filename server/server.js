99eec7d9a0cf98d3fb3e5b117ddc18487f601aaa
ScottHahn1 <scotthahn2210@gmail.com>
2024-11-26 17:17:26 +0200

    -added fixtures and results router -updated PORT fallback

--- server/server.js
+++ server/server.js
@@ -3,22 +3,24 @@ const cors = require('cors');
 const { highlightsRouter } = require('./routes/highlights');
 const dotenv = require('dotenv');
 const { newsRouter } = require('./routes/news');
-const { gamesRouter } = require('./routes/games');
 const { statsRouter } = require('./routes/stats');
 const { teamsRouter } = require('./routes/teams');
+const { fixturesRouter } = require('./routes/fixtures');
+const { resultsRouter } = require('./routes/results');
 
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8500;
 
 app.use(express.json());
 app.use(cors());
 
 app.use('/highlights', highlightsRouter);
 app.use('/news', newsRouter);
-app.use('/games', gamesRouter);
+app.use('/fixtures', fixturesRouter);
+app.use('/results', resultsRouter);
 app.use('/stats', statsRouter);
 app.use('/teams', teamsRouter);
 
-app.listen(PORT, () => console.log('Server started'));
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
