2c981eab1ff48b02e7456e8f975bab385dd09a3c
ScottHahn1 <scotthahn2210@gmail.com>
2024-11-26 17:12:07 +0200

    refactor: updated component logic from manually fetching data to using the useFetch hook

--- client/src/components/Highlights.js
+++ client/src/components/Highlights.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import parse from 'html-react-parser';
+import useFetch from '../hooks/useFetch';
 
 const Highlights = () => {
     const [videos, setVideos] = useState([]);
 
+    const { data } = useFetch('highlights');
+
     useEffect(() => {
-        fetch('https://premier-league-backend.vercel.app/highlights')
-        .then(response => response.json())
-        .then(data => {
-            setVideos(data.filter(item => item.competition.name === 'ENGLAND: Premier League'));
-        })
-    }, []);
+        data && setVideos(data.filter(item => item.competition.name === 'ENGLAND: Premier League'));
+    }, [data])
     
     return (  
         <div className='highlights-container'>
-            {videos.length > 0 && <h1 id='highlights'>Highlights</h1>}
+            {
+                videos.length > 0 && 
+                <h1 id='highlights'>
+                    Highlights
+                </h1>
+            }
             <div className='highlights'>
                 {videos.map((item, index) => (
                     <div key={index} className='highlight'> 
                         { parse(item.videos[0].embed.toString())} 
-                        <h4>{item.title}</h4>  
-                        {/* <hr></hr> */}
+                        <h4>
+                            {item.title}
+                        </h4>  
                         <p className='poppins-semiBold-italic' style={{color: 'rgb(233,0,82)'}}>
                             { new Date(item.date).toString().slice(0, 15) }
                         </p> 
