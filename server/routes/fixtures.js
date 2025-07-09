292d3e58575c309a13ea2c0e0134f4e1b1ffd593
ScottHahn1 <scotthahn2210@gmail.com>
2025-07-09 21:18:19 +0200

    feat: add route for fetching fixtures by round

--- server/routes/fixtures.js
+++ server/routes/fixtures.js
@@ -1,38 +1,69 @@
-const express = require('express');
-const dotenv = require('dotenv');
+import { Router } from 'express';
+import { config } from 'dotenv';
 
-dotenv.config();
+config();
 
-const fixturesRouter = express.Router();
+const fixturesRouter = Router();
 
 fixturesRouter.get('/', async (req, res) => {
     try {
-        const allRoundsData = [];
+        const response = await fetch('https://www.thesportsdb.com/api/v1/json/123/eventsnextleague.php?id=4328');
 
-        for (let round = 12; round <= 38; round++) {
-            const response = await fetch(`https://www.thesportsdb.com/api/v1/json/3/eventsround.php?id=4328&r=${round}&s=2024-2025`);
-            const data = await response.json();
-            await data.events && allRoundsData.push(...data.events);
-        }
+        if (!response.ok) {
+            throw new Error(`Failed to fetch fixtures: ${response.status} ${response.statusText}`)
+        };
+
+        const data = await response.json();
+        const round = data.events[0].intRound;
+
+        res.status(200).send(round);
+    }  catch(err) {
+        return res.status(500).json({ error: 'Failed to fetch fixtures data' });
+    }
+});
+
+fixturesRouter.get('/:round', async (req, res) => {
+    try {
+        const { round } = req.params;
+
+        const response = await fetch(`https://www.thesportsdb.com/api/v1/json/123/eventsround.php?id=4328&r=${round}&s=2025-2026`);
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch fixtures: ${response.status} ${response.statusText}`)
+        };
+
+        const data = await response.json();
  
-        const fixtures = allRoundsData.filter(event => {
-            return new Date(event.dateEvent).toISOString().slice(0, 10) >= new Date().toISOString().slice(0, 10);
-        })
+        const shortenNames = {
+            'Brighton and Hove Albion': 'Brighton',
+            'Wolverhampton Wanderers': 'Wolves',
+            'American Express Stadium': 'AMEX Stadium',
+            'Brentford Community Stadium': 'Brentford Stadium',
+            'Tottenham Hotspur Stadium': 'Tottenham Stadium',
+        };
 
-        let groupedFixtures = fixtures.reduce(function (a, b) {
+        const updatedFixtures = data.events.map(fixture => ({
+                ...fixture,
+                strHomeTeam: shortenNames[fixture.strHomeTeam] || fixture.strHomeTeam,
+                strAwayTeam: shortenNames[fixture.strAwayTeam] || fixture.strAwayTeam,
+                strVenue: shortenNames[fixture.strVenue] || fixture.strVenue
+            })
+        );
+
+        const groupedFixtures = updatedFixtures.reduce(function (a, b) {
             a[b.dateEvent] = a[b.dateEvent] || [];
             a[b.dateEvent].push(b);
             return a;
         }, Object.create(null));
 
-        let timeSortedFixtures = Object.values(groupedFixtures).map(fixture => {
+        const timeSortedFixtures = Object.values(groupedFixtures).map(fixture => {
             return fixture.sort((a, b) => a.strTime.localeCompare(b.strTime));
         });
-        
+
         res.status(200).send(timeSortedFixtures);
     }  catch(err) {
         return res.status(500).json({ error: 'Failed to fetch fixtures data' });
     }
 });
 
-module.exports = { fixturesRouter };
+export default fixturesRouter;
