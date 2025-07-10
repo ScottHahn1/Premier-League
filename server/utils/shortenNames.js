0f7a53df3d09cee984b4b567fb28c7d9aad81a68
ScottHahn1 <scotthahn2210@gmail.com>
2025-07-10 19:39:58 +0200

    feat: add shortened names mapping object

---
+++ server/utils/shortenNames.js
@@ -0,0 +1,7 @@
+export const shortenNames = {
+  'Brighton and Hove Albion': 'Brighton',
+  'Wolverhampton Wanderers': 'Wolves',
+  'American Express Stadium': 'AMEX Stadium',
+  'Brentford Community Stadium': 'Brentford Stadium',
+  'Tottenham Hotspur Stadium': 'Tottenham Stadium',
+};
