b58acfd868d5f7c414255943df858ed5f1e7c862
ScottHahn1 <scotthahn2210@gmail.com>
2025-07-10 19:28:09 +0200

    feat: add useFetchInfinite custom hook for fetching paginated data with useInfiniteQuery

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,22 +1,66 @@
 import { useQuery } from '@tanstack/react-query';
 
-const useFetch = (endpointPath, queryKey, staleTime) => {
-    const apiUrl = `https://premier-league-backend.vercel.app${endpointPath}`;
+const useFetch = (endpointPath, queryKey, enabled = true, staleTime) => {
+    const apiUrl = `http://localhost:8500${endpointPath}`;
 
     const fetchData = async () => {
         const res = await fetch(apiUrl);
+
+        if (!res.ok) {
+            throw new Error(`Fetch failed: ${res.status} ${res.statusText}`);
+        };
+
         const data = await res.json();
         return data;
     };
+// console.log('queryKey passed to useQuery:', queryKey);
 
-    const { data, isLoading, isError, error } = useQuery({
-        queryKey: [queryKey],
+    const { data, isLoading, isError, error, refetch } = useQuery({
+        queryKey,
         queryFn: fetchData,
+        enabled,
         cacheTime: 900000,
-        staleTime: staleTime && staleTime
+        staleTime: staleTime
     });
 
-    return { data, isLoading, isError, error };
-}
+    return { data, isLoading, isError, error, refetch };
+};
 
-export default useFetch;
+const useFetchInfinite = (endpointPath, queryKey, initialPage, getNextPage) => {
+    const fetchData = async ({ pageParam }) => {
+        const apiUrl = `http://localhost:8500${endpointPath}/${pageParam}`;
+
+        const res = await fetch(apiUrl);
+
+        if (!res.ok) {
+            throw new Error('Failed to fetch');
+        }
+
+        const data = await res.json();
+
+        return data;
+    };
+
+    const { 
+        data, 
+        isLoading,
+        fetchNextPage, 
+        hasNextPage, 
+        isFetchingNextPage 
+    } = useInfiniteQuery({
+        queryKey,
+        queryFn: fetchData,
+        enabled: !!initialPage,
+        initialPageParam: initialPage,
+        getNextPageParam: (lastPage) => {
+            if (lastPage?.length > 0) { 
+                return getNextPage(lastPage);
+            } 
+            return undefined;
+        },
+    });
+
+    return { data, isLoading, fetchNextPage, hasNextPage, isFetchingNextPage};
+};
+
+export { useFetch, useFetchInfinite };
