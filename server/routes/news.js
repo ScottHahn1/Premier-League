3374d2cc71eb1abf1698d4b830c695d961f7d97d
ScottHahn1 <scotthahn2210@gmail.com>
2025-07-18 21:50:02 +0200

    refactor: switch to ES module style imports and add check to see if data response was okay

--- server/routes/news.js
+++ server/routes/news.js
@@ -1,13 +1,18 @@
-const express = require('express');
-const dotenv = require('dotenv');
+import { Router } from 'express';
+import { config } from 'dotenv';
 
-dotenv.config();
+config();
 
-const newsRouter = express.Router();
+const newsRouter = Router();
 
 newsRouter.get('/', async (req, res) => {
     try {
         const response = await fetch('https://site.api.espn.com/apis/site/v2/sports/soccer/eng.1/news');
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch news: ${response.status} ${response.statusText}`);
+        };
+
         const data = await response.json();
         res.status(200).send(data);
     }  catch(err) {
@@ -18,6 +23,11 @@ newsRouter.get('/', async (req, res) => {
 newsRouter.get('/article', async (req, res) => {
     try {
         const response = await fetch(`https://gnews.io/api/v4/top-headlines?lang=en&q=football%20english%20premier%20league&token=${process.env.NEWS_API_KEY}`);
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch article: ${response.status} ${response.statusText}`)
+        };
+
         const data = await response.json();
         res.status(200).send(data);
     }  catch(err) {
@@ -25,4 +35,4 @@ newsRouter.get('/article', async (req, res) => {
     }
 });
 
-module.exports = { newsRouter };
+export default newsRouter;
