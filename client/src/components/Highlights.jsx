ebe19f8ecf8d9bc54c61575a39e125986acf3341
ScottHahn1 <scotthahn2210@gmail.com>
2025-07-28 17:44:13 +0200

    refactor: rename variables, enhance layout, and apply change styling

--- client/src/components/Highlights.jsx
+++ client/src/components/Highlights.jsx
@@ -3,30 +3,30 @@ import parse from 'html-react-parser';
 import { useFetch } from '../hooks/useFetch';
 
 const Highlights = () => {
-    const { data: videos } = useFetch('/highlights', ['highlights']);
+    const { data: highlights } = useFetch('/highlights', ['highlights']);
 
-    if (!videos?.length) {
+    if (!highlights?.length) {
         return null;
     }
 
     return (  
-        <div>
-            <h2 className='text-3xl font-semibold my-2'>
+        <div className='text-center md:text-left'>
+            <h2 className='text-4xl font-bold my-5'>
                 Highlights
             </h2>
 
-            <div className='bg-pink-800 my-2 flex flex-col items-center md:flex-row md:flex-wrap'>
-                {videos.map((item, index) => (
-                    <div key={index} className='my-4 w-full md:w-1/2 lg:w-1/3 bg-yellow-500'>  
-                        <div className='w-full md:w-[95%]'>
-                            { parse(item.videos[0].embed.toString())} 
+            <div className='my-2 flex flex-col items-center md:gap-2 md:flex-row md:flex-wrap xl:gap-4'>
+                {highlights.map((highlight, index) => (
+                    <div key={`${highlight.title}-${index}`} className='my-2 w-full md:w-[30%]'>  
+                        <div className='w-full '>
+                            { parse(highlight.videos[0].embed.toString())} 
                         </div>
 
                         <span className='line-clamp-1 lg:w-[95%]'>
-                            {item.title}
+                            {highlight.title}
                         </span>  
-                        <p className='poppins-semiBold-italic text-pink'>
-                            { new Date(item.date).toString().slice(0, 15) }
+                        <p className='poppins-semiBold-italic text-hotpink'>
+                            { new Date(highlight.date).toString().slice(0, 15) }
                         </p> 
                     </div>
                 ))}
