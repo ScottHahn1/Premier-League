7bf80593ac5c6aab7c69b7eff7a4f06e698aacca
ScottHahn1 <scotthahn2210@gmail.com>
2025-07-28 17:45:38 +0200

    refactor: fetch round and fixtures separately, update layout and remove unused logic

--- client/src/components/HomeFixtures.jsx
+++ client/src/components/HomeFixtures.jsx
@@ -1,33 +1,51 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
-import GetTeams from './GetTeams';
-import RenderHomeFixtures from './RenderHomeFixtures';
-import useFetch from '../hooks/useFetch';
+import { useFetch } from '../hooks/useFetch';
 
 const HomeFixtures = () => {
-    const [teams, setTeams] = useState([]);
+    const { data: round, isLoading: roundLoading } = useFetch('/fixtures', ['homeFixturesRound']);
 
-    const { data: fixtures, isLoading } = useFetch('/fixtures', 'homeFixtures');
+    const { data: fixtures, isLoading: fixturesLoading } = useFetch(`/fixtures/${round}`, ['homeFixtures', round], !!round);
+
+    if (roundLoading || fixturesLoading || !round || !fixtures) {
+        return null;
+    }
 
     return (
-        <div className='home-fixtures-main'>
+        <div className='bg-black text-white text-center ml-2'>
+            <h1 className='text-pink-600 text-2xl font-medium'>
+                Upcoming Fixtures
+            </h1> 
+
             {
-                fixtures &&
-                <>
-                    <GetTeams setTeams={setTeams} />
-                    <h2 style={{color: '#e90052'}}>
-                        Upcoming Fixtures
-                    </h2> 
-                    <img 
-                        src='https://www.logolynx.com/images/logolynx/c8/c84557602b44e25f665a6ec4bbf7691c.png' 
-                        alt='Premier League' 
-                        width='150px' 
-                    />
-                    <RenderHomeFixtures fixtures={fixtures} teams={teams} loading={isLoading}  />
-                </>
+                fixtures.map((group, groupIndex) => (
+                    <div key={groupIndex} className='text-center flex flex-col gap-2 mt-2'>
+                        <h2>{new Date(group[0].dateEvent).toDateString()}</h2>
+                        {
+                            group.map(fixture => (
+                                <div key={fixture.idEvent} className='grid grid-cols-5 gap-1 items-center'>
+                                    <span>{fixture.strAbbHomeTeam}</span>
+                                    <img 
+                                        className='w-8 lg:w-12' 
+                                        src={fixture.strHomeTeamBadge} 
+                                        alt={`${fixture.strHomeTeam}'s club badge`}
+                                    />
+                                    <span>
+                                        { Array.from(fixture.strTime).slice(0, 5) }
+                                    </span>
+                                    <img 
+                                        className='w-8 lg:w-12' 
+                                        src={fixture.strAwayTeamBadge} 
+                                        alt={`${fixture.strAwayTeam}'s club badge`}
+                                    />
+                                    <span>{fixture.strAbbAwayTeam}</span>
+                                </div>
+                            ))
+                        }
+                    </div>
+                ))
             }
         </div>
-    )
+    );
 };
 
 export default HomeFixtures;
