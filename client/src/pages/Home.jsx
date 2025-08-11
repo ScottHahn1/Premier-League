f55d36d644db98da050edf66e8b44388733ba305
ScottHahn1 <scotthahn2210@gmail.com>
2025-08-11 22:40:52 +0200

    refactor: switch Home component to Tailwind styling and added useFetch hook for data fetching

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,57 +1,46 @@
 import React, { useEffect, useState } from 'react';
-import '../styles/Home.css';
 import News from '../components/News';
 import Highlights from '../components/Highlights';
 import HomeTable from '../components/HomeTable';
 import HomeFixtures from '../components/HomeFixtures';
+import { useFetch } from '../hooks/useFetch';
 
 const Home = () => {
-    const [article, setArticle] = useState();
+    const { data: article, isLoading } = useFetch('/news/article', ['mainArticle']);
 
-    useEffect(() => {
-        fetch('https://premier-league-backend.vercel.app/news/article')
-        .then(response => response.json())
-        .then(data => {
-            setArticle(data.articles[Math.floor(Math.random() * data.articles.length)]);
-        })
-    }, []);
+    if (!article || isLoading) {
+        return null;
+    }
 
     return (
-        <section className='home'>
-            <div className='home-main-container'>
-                {
-                    article && (
-                    <>
-                        {
-                            article.image !== null && 
-                            <img 
-                                id='headline-img' 
-                                src={article.image} 
-                                onClick={() => window.open(article.url)} alt='Headline article thumbnail' 
-                            />
-                        }
+        <section>
+            <div className='mt-4 bg-[linear-gradient(270deg,_rgb(255,40,130)_30%,_rgb(255,105,0))] flex justify-end'>
+                    <div className='flex gap-1 justify-center'>
+                        <img 
+                            className='h-96'
+                            src={article.image} 
+                            onClick={() => article.image !== null && window.open(article.url)} 
+                            alt='Headline article' 
+                        />
 
-                        <div className='home-main'>
-                            <h4 style={{color: 'rgb(4,245,255)'}}>Top Story</h4>
-                            <h2>{article.title}</h2>
+                        <div className='px-2 w-2/6 text-white flex flex-col justify-around bg-[rgba(56,0,60)] bg-[url("https://www.premierleague.com/resources/prod/v6.101.4-4472/i/bg-elements/heading-bg-desktop.svg")]'>
+                            <span className='text-cyan-600 font-bold'>Top Story</span>
+                            <h1 className='text-xl font-medium'>{article.title}</h1>
                             <p>{article.description}</p>
-                            <h4>Source - {article.source.name}</h4>
+                            <span>Source - {article.source.name}</span>
                         </div>
-                    </>
-                    )
-                }
-
+                    </div>
             </div>
             
-            <div className='home-content'>
-                <div className='fixtures-table'>
-                    <HomeFixtures />
-                    <HomeTable />
+            <div className='md:flex md:gap-4 md:w-full'>
+                <div className='hidden lg:block xl:w-[20%]'>
+                    <HomeFixtures /> 
+                    <HomeTable /> 
                 </div>
 
-                <div className='highlights-news'>
-                    <Highlights /> &nbsp;
-                    <News />
+                <div className='w-full md:ml-10 lg:ml-0 lg:w-[75%] xl:w-[80%]'>
+                    <Highlights />
+                    <News /> 
                 </div>
             </div>
         </section>
